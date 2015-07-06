ba9df02b24487cb6fba7946e3e93ebec086e9dd3
Gilad Gurantz <gilad@lyft.com>
2015-07-05 19:52:43 -0700

    Initial Commit

---
+++ ArabicDictionary/ArabicDictionary.h
@@ -0,0 +1,19 @@
+//
+//  ArabicDictionary.h
+//  ArabicDictionary
+//
+//  Created by Gilad Gurantz on 7/5/15.
+//  Copyright (c) 2015 Lazy Arcade. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ArabicDictionary.
+FOUNDATION_EXPORT double ArabicDictionaryVersionNumber;
+
+//! Project version string for ArabicDictionary.
+FOUNDATION_EXPORT const unsigned char ArabicDictionaryVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ArabicDictionary/PublicHeader.h>
+
+
